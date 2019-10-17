4624d9871ddd29f12ec4f5ff247aa330193d3fa8
Bernd Niklas <niklas@solutio.de>
2019-10-18 00:45:43 +0200

    Die ersten Welten werden generiert

--- apps/api/src/app/create-world/create-world.controller.ts
+++ apps/api/src/app/create-world/create-world.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppService } from '../app.service';
-import { Dice, GamePref } from '@galaxy/game-objects';
+import { Dice, GamePref, World } from '@galaxy/game-objects';
+
+const fs = require('fs');
 
 @Controller('create-world')
 export class CreateWorldController {
@@ -8,15 +10,31 @@ export class CreateWorldController {
 
     @Get('CreateWorld')
     getData(): String {
+        const rawdata = fs.readFileSync('gamePref.json');
+        const gamepref:GamePref = JSON.parse(rawdata);
         const dice:Dice = new Dice;
-        dice.sides = 6
-        return `Hallo CreateWorld ${dice.roll ()}`;
+        const worldCount = gamepref.worldCount;
+        const worlds:World[] = new Array;
+
+        dice.sides = worldCount
+        let outString = '';
+
+        for (let i = 0; i < worldCount; i++) {
+          const world: World = new World;
+          world.setNumber(dice.roll());
+          worlds.push(world);
+
+          outString += `${world.name}(${dice.roll()}, ${dice.roll()}, ${dice.roll()}) [Bernd] (D-Schiffe=${dice.roll()})\n`;
+        }
+        const data = JSON.stringify(worlds);
+        fs.writeFileSync('worlds.json', data);
+        fs.writeFileSync('worlds.txt', outString);
+        return outString;
       }
 
     @Post('SetGamePref')
     setGamePref(@Body() gamepref: GamePref) {
       const data = JSON.stringify(gamepref);
-      const fs = require('fs');
 
       fs.writeFileSync('gamePref.json', data);
 
--- libs/game-objects/src/lib/world.ts
+++ libs/game-objects/src/lib/world.ts
@@ -1,4 +1,9 @@
 export class World {
     number: Number = 0
     name: String = `W${this.number}`;
+
+    setNumber(aNumber:Number) {
+        this.name = `W${this.number}`;
+        this.number = aNumber;
+    }
 }
