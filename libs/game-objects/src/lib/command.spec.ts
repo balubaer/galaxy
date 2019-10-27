e243d47dbc8275dc0cb1a1d3989e0bd852857205
Bernd Niklas <niklas@solutio.de>
2019-10-27 17:20:49 +0100

    MoveCommand implementiert

--- libs/game-objects/src/lib/command.spec.ts
+++ libs/game-objects/src/lib/command.spec.ts
@@ -1,10 +1,31 @@
-import { Command, TurnPhase } from "./command";
+import { Command, TurnPhase, MoveCommand } from "./command";
 import { Player } from './player';
+import { World, TestWorldsArrayFactory } from '..';
+import { Fleet } from './fleet';
 
 const command = new Command('F1W2', new Player('ZAPHOD'), TurnPhase.Movement);
-
+const worlds: Array<World> = new TestWorldsArrayFactory().worlds;
+const world4 = worlds[3];
+const world2 = worlds[1];
+const fleet4 = world4.fleets[0];
+const player = world4.player;
+const moveWorldArray = new Array();
+moveWorldArray.push(world2);
+const moveCommand: MoveCommand = new MoveCommand(fleet4, world4, moveWorldArray, 'F4W2', player);
 describe('Command', () => {
-    it('should create an instance', () => {
-      expect(command).toBeTruthy();
-    });
+  it('should create an instance', () => {
+    expect(command).toBeTruthy();
   });
+});
+
+describe('MoveCommand', () => {
+  it('should create an instance', () => {
+    expect(moveCommand).toBeTruthy();
+  });
+  it('test executeCommand', () => {
+    moveCommand.executeCommand();
+    const fleetMovement = fleet4.fleetMovements[0];
+    expect(fleetMovement.fromWorld).toBe(world4);
+    expect(fleetMovement.toWorld).toBe(world2);
+  });
+});
--- libs/game-objects/src/lib/command.ts
+++ libs/game-objects/src/lib/command.ts
@@ -1,4 +1,7 @@
 import { Player } from './player'
+import { Fleet } from './fleet';
+import { World } from './world';
+import { FleetMovement } from './fleet-movement';
 
 export interface ExecuteCommand {
     executeCommand(): void;
@@ -22,10 +25,74 @@ export class Command {
     player: Player;
     //errors: Array<T>;
     turnPhase: number;
-    
+
     constructor(aString: string, aPlayer: Player, aTurnPhase: number) {
         this.string = aString;
         this.player = aPlayer;
         this.turnPhase = aTurnPhase;
+    }
+}
+
+//FnnnWmmm FnnnWmmmWooo FnnnWmmmWoooWrrr
+export class MoveCommand extends Command implements ExecuteCommand {
+    fleet: Fleet
+    worlds: Array<World>;
+    homeWorld: World;
+    count = 0;
+
+    constructor(aFleet: Fleet, aHomeWorld: World, aWorldArray: Array<World>, aString: string, aPlayer: Player) {
+        super(aString, aPlayer, TurnPhase.Movement);
+        this.fleet = aFleet;
+        this.homeWorld = aHomeWorld;
+        this.worlds = aWorldArray;
+    }
+
+    executeCommand() {
+        if (this.player.stringName() === this.fleet.player.stringName()) {
+            let fromWorld: World = this.homeWorld;
+            let toWorld: World;
+            let isError = false
+            for (const world of this.worlds) {
+                toWorld = world;
+                if (fromWorld.hasConnectionToWorld(toWorld)) {
+                    fromWorld = world;;
+                } else {
+                    //TODO: Fehler
+                    isError = true;
+                    break;
+                }
+
+                if (this.fleet.ships === 0) {
+                    isError = true;
+                }
+
+                if (isError === false) {
+                    if (this.fleet.fired) {
+                        isError = true;
+                    }
+                }
+            }
+
+            if (isError === false) {
+                fromWorld = this.homeWorld;
+
+                for (const toWorldFromWorlds of this.worlds) {
+                    toWorld = toWorldFromWorlds;
+                    const fleetMovement = new FleetMovement();
+                    const fleetCopy = new Fleet();
+                    fleetCopy.player = this.fleet.player;
+                    fleetCopy.number = this.fleet.number;
+                    fleetMovement.fleet = fleetCopy;
+                    fleetMovement.toWorld = toWorld;
+                    fleetMovement.fromWorld = fromWorld;
+
+                    this.fleet.fleetMovements.push(fleetMovement);
+
+                    fromWorld = toWorld;
+                }
+            }
+        } else {
+            //TODO: Fehler Flotte ist nicht vom Spieler
+        }
     }
 }
