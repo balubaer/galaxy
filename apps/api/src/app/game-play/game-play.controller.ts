a6feb773a28cdc2fcbe0b7a833f89d5e9cccab83
Bernd Niklas <bernd_niklas@web.de>
2019-11-04 07:43:37 +0100

    RestApi um game-play erweitert Fortsetzung

--- apps/api/src/app/game-play/game-play.controller.ts
+++ apps/api/src/app/game-play/game-play.controller.ts
@@ -1,13 +1,26 @@
-import { Controller, Get, Body } from '@nestjs/common';
+import { Controller, Get, Body, Query } from '@nestjs/common';
 import { readFileSync } from 'fs';
 import { RequestTurnData } from '@galaxy/game-objects';
 
 @Controller('game-play')
 export class GamePlayController {
     @Get('GetTurnData')
-    getTurnData(@Body() request: RequestTurnData): Array<string> {
-        //const stringData = readFileSync('gamePref.json', 'utf8');
-       // const gamepref: GamePref = JSON.parse(stringData);
+    getTurnData(@Body() request: RequestTurnData, @Query() query): Array<string> {
+        console.log('Query: ' + query);
+        console.log('Query.test: ' + query['Test']);
+        console.log('request:' + request);
+        console.log('request.turn:' + request.turn);
+        console.log('request.playerName:' + request.playerName);
+        
+        return ['Hallo1', 'Hallo2'];
+    }
+
+    @Get('GetTurnData2')
+    getTurnData2(@Body() request: RequestTurnData): Array<string> {
+      
+        console.log('request.turn:' + request.turn);
+        console.log('request.playerName:' + request.playerName);
+        
         return ['Hallo1', 'Hallo2'];
     }
 }
